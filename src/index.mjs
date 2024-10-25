f011f6a9cb99b8b30a2057aafeafabc99c18540f
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-10-25 12:15:22 +0200

    topic: get request

--- src/index.mjs
+++ src/index.mjs
@@ -7,6 +7,21 @@ const app = express();
 // PORT is a variable that stores the port number to use for the server
 const PORT = process.env.PORT || 3000;
 
+// the first parameter is the route
+// the second parameter is a callback function that will be called when the route is requested
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "andrea", displayName: "Andrea" },
+    { id: 2, username: "mark", displayName: "Mark" },
+    { id: 3, username: "larry", displayName: "Larry" },
+  ]);
+});
+app.get("/api/products", (req, res) => {
+  res.send([{ id: 1, productName: "berry", price: 2.99 }]);
+});
 // the second parameter is a callback function that will be called when the server starts
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
