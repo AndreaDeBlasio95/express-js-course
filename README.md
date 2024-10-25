28f8ff873b50d3678a71f672fa4810fa968d420a
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-10-26 01:23:55 +0200

    Query Params, Post Requests, Put Requests

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # express-js-course
- Express JS Full Course
+
+Express JS Full Course
+
+npm init -y
+will create a packega.json file
+
+npm i express
+install express
+
+npm i -D nodemon
+install nodemon as a Dev dependencie
+
+Add these two lines in package.json -> scripts ->  
+ "start:dev": "nodemon ./src/index.js",
+"start": "node ./src/index.js"
+
+Add this line at the end on the package.json
+"type": "module"
+This allow the modern import export syntax
--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 const mockUsers = [
   { id: 1, username: "andrea", displayName: "Andrea" },
   { id: 2, username: "mark", displayName: "Mark" },
@@ -12,15 +15,44 @@ const mockUsers = [
 // PORT is a variable that stores the port number to use for the server
 const PORT = process.env.PORT || 3000;
 
+// the second parameter is a callback function that will be called when the server starts
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 // the first parameter is the route
 // the second parameter is a callback function that will be called when the route is requested
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
 // Get all users
 app.get("/api/users", (req, res) => {
-  res.send(mockUsers);
+  console.log(req.query);
+  // destructuring
+  const {
+    query: { filter, value },
+  } = req;
+
+  // when filter and value are undefined
+  if (!filter && !value) return res.send(mockUsers);
+
+  // when filter is defined and value is defined
+  if (filter && value)
+    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+
+  return res.send(mockUsers);
 });
+
+// Post
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const { body } = req;
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
+});
+
 // Get a specific user
 app.get("/api/users/:id", (req, res) => {
   // req.params is an object that stores all the route parameters
@@ -42,7 +74,24 @@ app.get("/api/users/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.send([{ id: 1, productName: "berry", price: 2.99 }]);
 });
-// the second parameter is a callback function that will be called when the server starts
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+// PUT: Update a specific user
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return res.sendStatus(200);
 });
+
+// PUT: Update the entire user, all fields, when not all fields are provided, they will be set to null
+// PATCH: Update a portion of a specific user
+// DELETE: Delete a specific user
