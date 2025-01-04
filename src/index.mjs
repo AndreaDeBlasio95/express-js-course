e9fa8e33539d8634d06ee0396be830f472d72f63
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2025-01-04 12:43:14 +0100

    sessions completed

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -36,6 +37,42 @@ app.get("/", (req, res) => {
   req.session.visited = true; // setting a session variable called0o visited
   res.cookie("hello", "world", { maxAge: 30000, signed: true });
   res.status(201).send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "BAD CREDENTIALS" });
+  req.session.user = findUser;
+  res.status(200).send({ msg: "Logged in" });
+});
+
+app.get("/api/auth/status", (req, res) => {
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+
+  const { body: item } = req;
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.send(req.session.cart ?? []);
 });
 
 /** * |----- Middleware -----|
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,5 +1,5 @@
 export const mockUsers = [
-  { id: 1, username: "andrea", displayName: "Andrea" },
-  { id: 2, username: "mark", displayName: "Mark" },
-  { id: 3, username: "larry", displayName: "Larry" },
+  { id: 1, username: "andrea", displayName: "Andrea", password: "dev" },
+  { id: 2, username: "mark", displayName: "Mark", password: "dev" },
+  { id: 3, username: "larry", displayName: "Larry", password: "dev" },
 ];
